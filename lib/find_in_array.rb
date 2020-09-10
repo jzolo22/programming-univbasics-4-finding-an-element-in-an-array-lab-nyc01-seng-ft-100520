0fb4cbcfac04a745a97909a705f31c2467f5f967
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-10 02:45:20 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,5 +4,12 @@
 
 def find_element_index(array, value_to_find)
   i = 0
-  while i < 
+  while i < array.length do
+    if array[i] == value_to_find
+      return i
+    else
+      i +=1
+    end
+  end
+  return nil
 end
